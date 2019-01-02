718593fce1fe6041b3561754f3484c3bb59bfa56
denis96z <zinoviev.denis@yandex.ru>
2019-01-02 17:01:29 +0300

    db connection added

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module tp-project-db
 
 require (
 	github.com/fasthttp/router v0.2.0
+	github.com/jackc/pgx v3.3.0+incompatible
 	github.com/mailru/easyjson v0.0.0-20180823135443-60711f1a8329
+	github.com/pkg/errors v0.8.0 // indirect
 	github.com/valyala/fasthttp v1.0.0
 )
--- repositories/connection.go
+++ repositories/connection.go
@@ -1,4 +1,36 @@
 package repositories
 
+import (
+	"github.com/jackc/pgx"
+)
+
 type Connection struct {
+	conn   *pgx.ConnPool
+	config pgx.ConnConfig
+}
+
+func NewConnection() *Connection {
+	config, err := pgx.ParseEnvLibpq()
+	if err != nil {
+		panic(err)
+	}
+
+	return &Connection{
+		config: config,
+	}
+}
+
+func (c *Connection) Open() error {
+	var err error
+	c.conn, err = pgx.NewConnPool(
+		pgx.ConnPoolConfig{
+			ConnConfig: c.config,
+		},
+	)
+	return err
+}
+
+func (c *Connection) Close() error {
+	c.conn.Close()
+	return nil
 }
